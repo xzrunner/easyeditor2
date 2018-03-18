1209353fb745a2af60a6195a14b3ec879208a059
zz <zhuguang@ejoy.com>
2018-03-18 23:02:33 +0800

    [CHANGED] pass scene node

--- include/ee2/WxCompScriptPanel.h
+++ include/ee2/WxCompScriptPanel.h
@@ -2,6 +2,8 @@
 
 #include <ee0/WxCompPanel.h>
 
+#include <node0/typedef.h>
+
 namespace n2 { class CompScript; }
 
 class wxTextCtrl;
@@ -12,7 +14,8 @@ namespace ee2
 class WxCompScriptPanel : public ee0::WxCompPanel
 {
 public:
-	WxCompScriptPanel(wxWindow* parent, n2::CompScript& cscript);
+	WxCompScriptPanel(wxWindow* parent, n2::CompScript& cscript,
+		const n0::SceneNodePtr& node);
 
 	virtual void RefreshNodeComp() override;
 
@@ -22,7 +25,8 @@ private:
 	void OnSetFilepath(wxCommandEvent& event);
 
 private:
-	n2::CompScript& m_cscript;
+	n2::CompScript&  m_cscript;
+	n0::SceneNodePtr m_node;
 
 	wxTextCtrl* m_filepath;
 
